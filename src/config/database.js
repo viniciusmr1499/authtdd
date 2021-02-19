8a7b2a7d1d75a21dfe718b3a698b207387378dd1
Marcos  Oliveira <marcos.oliveira@fortbrasil.com.br>
2021-02-19 12:14:03 -0300

    initial commit

--- src/config/database.js
+++ src/config/database.js
@@ -1,8 +1,8 @@
 module.exports = {
     host: process.env.HOST_API,
     username: process.env.API_USER,
-    password:process.env.API_PASS,
-    dialect: 'postgres',
+    password: process.env.API_PASS,
+    dialect: process.env.API_DIALECT,
     operatorsAliases: false,
     logging: false,
     define: {
